3f634b8414fee0c53df049d6de4aae739bd17eae
ReemaAlzaid <439201824@student.ksu.edu.sa>
2021-09-12 17:43:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/DisplayMessage.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView= findViewById(R.id.textView2);
+        textView.setText(message);
+    }
+}
