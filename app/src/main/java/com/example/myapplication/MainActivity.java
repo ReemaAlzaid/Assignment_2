b333f2f7ee41ccd5306200aacd2c83a81e855da9
ReemaAlzaid <439201824@student.ksu.edu.sa>
2021-09-21 03:55:37 +0300

    An app to display a name.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,35 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "Hello";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle saved){
+        super.onCreate(saved);
         setContentView(R.layout.activity_main);
     }
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessage.class);
+    public void Viewmessage(View view) {
+        //to hide the keyboard when SUBMIT is pressed
+        InputMethodManager input = (InputMethodManager) getSystemService(MainActivity.INPUT_METHOD_SERVICE);
+        input.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+
         EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
         String message = "Hello, "+(editText.getText().toString());
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        TextView textView= findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
